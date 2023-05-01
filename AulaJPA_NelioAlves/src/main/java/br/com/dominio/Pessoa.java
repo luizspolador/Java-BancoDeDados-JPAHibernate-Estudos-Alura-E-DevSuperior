94c13f50a75f3d24e24614dfea319dce9c30b3f9
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-05-01 15:14:53 -0300

    conexao com DB utilizando JPA - hibernate

---
+++ AulaJPA_NelioAlves/src/main/java/br/com/dominio/Pessoa.java
@@ -0,0 +1,54 @@
+package br.com.dominio;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nome_completo")
+    private String nome;
+    private String email;
+
+    public Pessoa(){
+    }
+
+    public Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + getId() + ", Nome: " + getNome() + ", E-mail: " + getEmail();
+    }
+}
